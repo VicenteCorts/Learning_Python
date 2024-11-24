8c9aeb24352a1176a9fa7cf2cd0022c6218cdc47
VicenteCorts <vicentevoley9@gmail.com>
2024-11-24 19:41:34 +0100

    Clase 30 - ejercicio

--- section4.py
+++ section4.py
@@ -8,6 +8,25 @@ print("totai".replace("i","y"))
 # Clase 30
 '''
 monday_temperature = [9.1, 8.8, 7.5]
+
+# Append Item to List (E)
+## Append the value of current to the end of the list seconds Please use the list.append() method to do that.
+seconds = [1.23, 1.45, 1.02]
+current = 1.11
+seconds.append(current)
+
+# Remove Item from List (E)
+## Remove item 1.45 from seconds.
+seconds = [1.23, 1.45, 1.02, 1.11]
+seconds.remove(1.45)
+
+# Remove Three Items From List (E)
+## Remove items 1.45, 1.02, and 1.11 from seconds.
+seconds = [1.23, 1.45, 1.02, 1.11]
+new=seconds[0]
+seconds.clear()
+seconds.append(new)
+print(seconds)
 '''
 
 # Clase 31
