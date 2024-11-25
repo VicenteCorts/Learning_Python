b2f6c5bb035154c2566cab411648e1f9fb7b9bc0
VicenteCorts <vicentevoley9@gmail.com>
2024-11-25 20:58:27 +0100

    Clase 56 - ejercicios

--- section6.py
+++ section6.py
@@ -19,12 +19,29 @@ print(message)
 '''
 
 # Clase 55
+'''
 name = input("Nombre: ")
 surname = input("Apellido: ")
 message = "Hola: %s %s" % (name, surname)
 message = f"Hola mi compadre: {name} {surname}"
 print(message)
+'''
 
+# Clase 56
 
+# Formatting Strings
+## Implement a function that gets a person's name as a parameter and greets the person with Hi Person. For example, if I called your function using foo("Marry") the function should return Hi Marry .
+def foo(name):
+   return f"Hi {name}"
 
+print(foo("Mary"))
+
+# Formatting and Uppercase
+## Implement a function that gets a person's name (e.g. john) as a parameter and returns a greeting (e.g. Hi John). Note that the first letter of the person's name should always be uppercase.
+def foo(name):
+    uppername = name.title()
+    msg = f"Hi {uppername}"
+    return msg
+    
+print(foo("aaaaaa"))
 
