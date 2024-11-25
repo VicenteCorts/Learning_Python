5dc5de3399a79ab1295db37561b5a3d5dbc013f6
VicenteCorts <vicentevoley9@gmail.com>
2024-11-25 20:00:58 +0100

    Clase 50 - ejercicio

--- section5.py
+++ section5.py
@@ -116,3 +116,19 @@ if 3 > 1:
 print('cc')
 print('ccc')
 
+# Hot, Warm, Cold (E)
+## Define a function that: (1) takes a temperature as a parameter (2) returns "Hot" if the temperature is greater than 25 (3) returns "Warm" if the temperature is between 15 and 25, including 15 and 25. (4) returns "Cold" if the temperature is less than 15. If I called your function with foo(10) it would return "Cold". foo(15) or foo(16) or foo(25) would all return "Warm" and foo(26) would return "Hot".
+def foo(temp):
+    if temp > 25:
+        msg = "Hot"
+    elif temp <= 25 and temp >= 15:
+        msg = "Warm"
+    else:
+        msg = "Cold"
+    return msg
+    
+print(foo(27))
+print(foo(25))
+print(foo(23))
+print(foo(15))
+print(foo(12))
