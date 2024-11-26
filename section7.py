29c8831de6ac3d98d114736244e891994c634422
VicenteCorts <vicentevoley9@gmail.com>
2024-11-26 10:53:14 +0100

    Clase 59 - ejercicio

--- section7.py
+++ section7.py
@@ -7,7 +7,25 @@ for temp in monday_temperature:
 for letter in "Hello":
     print(letter)
 
+# Loop Over Colors (E)
+## Loop over the colors items and print out the item in every loop. 
+colors = [11, 34, 98, 43, 45, 54, 54]
+for color in colors:
+    print(color)
 
+# Loop Over Big Colors (E)
+## Loop over the colors items and print out the item in every loop only if the item is greater than 50. 
+colors = [11, 34, 98, 43, 45, 54, 54]
+for color in colors:
+    if color>50:
+        print(color)
+
+# Loop Over Integer Colors (E)
+## Loop over the colors items and print out the item in every loop only if the item is an integer.
+colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
+for color in colors:
+    if type(color) == int:
+        print(color)
 
 # Clase 60
 
