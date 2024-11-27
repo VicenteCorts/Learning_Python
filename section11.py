29156fe12dc4cd0e47c1e2f741e1ab156160f9c2
VicenteCorts <vicentevoley9@gmail.com>
2024-11-27 12:07:10 +0100

    Clase 87 - ejercicio

--- section11.py
+++ section11.py
@@ -2,8 +2,10 @@
 myfile = open("downloads/fruits.txt")
 print(myfile.read())
 
-
-
+# Read Text From File and Print (E)
+## On the side panel there's a bear.txt file. The existing code opens that file in read mode. Below that code, please read the file content and print out the content.
+file = open("bear.txt")
+print(file.read())
 
 
 
