68171ee14c7961fc3685eecde00695b1cabb1bbb
VicenteCorts <vicentevoley9@gmail.com>
2024-11-27 11:40:03 +0100

    Clase 82 - ejercicio

--- section10.py
+++ section10.py
@@ -24,9 +24,18 @@ def mean(*args):
     return sum(args)/len(args)
 print(mean(1, 6, 3))
 
+# Average Function (E)
+## Define a function that takes an indefinite number of numbers as arguments and returns their average. If I called your function with foo(10, 20, 30, 40) it should return the 25, the average of those numbers.
+def foo(*args):
+    return sum(args)/len(args)
+print(foo(10, 20, 30, 40) )
 
-
-
+# Indefinite Number of Strings Processed (E)
+## Define a function that takes an indefinite number of strings as parameters and returns a list containing all the strings in UPPERCASE and sorted alphabetically. For example, if I called your function with foo("snow", "glacier", "iceberg") it should return ["GLACIER", "ICEBERG", "SNOW"].
+def foo(*args):
+    return sorted(arg.upper() for arg in args)
+    
+print(foo("snow", "glacier", "iceberg"))
 
 
 
